e2b6af0aae41f3efa3417a21881c1aee3ca99a1d
BISWAP9 <116273370+BISWAP9@users.noreply.github.com>
2022-12-01 23:20:45 +0300

    getUser

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const Friends = () => {
   return <h2>Друзья</h2>;
 };
 
-function App() {
+function App(props) {
   return (
     <div className="container-fluid mt-5">
 <div className="row">
@@ -39,7 +39,7 @@ function App() {
 </div>
 <div className="col-sm-9">
 <Routes>
-  <Route path='/profile' element={<Profile />} />
+  <Route path='/profile' element={<Profile function={props.function} />} />
   <Route path='/messages' element={<Messages/>} />
   <Route path='/settings' element={<Settings/>} />
   <Route path='/friends' element={<Friends/>} />
