234ad55fdc6c4e6ee683e79c6ecfa56aa18b8fb1
BISWAP9 <116273370+BISWAP9@users.noreply.github.com>
2022-12-01 23:20:11 +0300

    function getUser

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,17 +1,19 @@
 import styles from "./Profile.module.css";
 
 
-const Profile = () => {
+const Profile = (props) => {
+	let user = props.function();
+	// console.log(user);
   return (
 		<div className="row">
 			<div className="col-sm-3">
-				<img src="https://www.seekpng.com/png/full/356-3562377_personal-user.png" alt="" width={"100%"}/>
+				<img src={user.avatar} alt="" width={"100%"}/>
 			</div>
 			<div className="col-sm-9">
-<h1 style={{backgroundColor: "lightblue"}}>Фамилия и имя</h1>
-<h2 className={styles.about}>Рассказ о себе</h2>
-<p>Email: ivan@ya.ru</p>
-<p>Id: 2</p>
+<h1 style={{backgroundColor: "lightblue"}}>{user.name} {user.lastname}</h1>
+<h2 className={styles.about}>{user.about}</h2>
+<p>Email: {user.email}</p>
+<p>Id: {user.id}</p>
 <p className={styles.paragraph}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur aperiam veritatis repellat doloremque beatae! Quisquam odit atque tempore a id, quod sit illo quam nemo, culpa voluptatum dolorum corporis facilis!</p>
 			</div>
 		</div>
