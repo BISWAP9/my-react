dec0383d9a5ba13da2dc385e87615ac672d42c6e
BISWAP9 <116273370+BISWAP9@users.noreply.github.com>
2022-12-03 22:41:02 +0300

    settings advise, add shop

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,67 @@
-import { NavLink, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Profile from './components/Profile';
-import Messages from './components/Messages';
-import Friends from './components/Friends';
-import Settings from './components/Settings';
+import { NavLink, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Profile from "./components/Profile";
+import Messages from "./components/Messages";
+import Friends from "./components/Friends";
+import Settings from "./components/Settings";
 
 function App(props) {
-  return (
-    <div className="container-fluid mt-5">
-<div className="row">
-<div className="col-sm-3">
-<div className="nav flex-column nav-pills" aria-orientation="vertical">
-<NavLink className="nav-link " to="profile">
-Профиль
-</NavLink>
-<NavLink className="nav-link" to="messages">
-Сообщения
-</NavLink>
-<NavLink className="nav-link" to="settings">
-Настройки
-</NavLink>
-<NavLink className="nav-link" to="friends">
-Мои друзья
-</NavLink>
-</div>
-</div>
-<div className="col-sm-9">
-<Routes>
-  <Route path='/profile/*' element={<Profile function={props.function.key_getUser} />} />
-  <Route path='/messages' element={<Messages/>} />
-  <Route path='/settings' element={<Settings/>} />
-  <Route path='/friends' element={<Friends function={props.function.key_getUsers}/>} />
-</Routes>
-</div>
-</div>
-</div>
-  );
+    return (
+        <div className="container-fluid mt-5">
+            <div className="row">
+                <div className="col-sm-3">
+                    <div
+                        className="nav flex-column nav-pills"
+                        aria-orientation="vertical"
+                    >
+                        <NavLink className="nav-link " to="profile">
+                            Профиль
+                        </NavLink>
+                        <NavLink className="nav-link" to="messages">
+                            Сообщения
+                        </NavLink>
+                        <NavLink className="nav-link" to="settings">
+                            Настройки
+                        </NavLink>
+                        <NavLink className="nav-link" to="friends">
+                            Мои друзья
+                        </NavLink>
+                        <NavLink className="nav-link" to="shop">
+                            Магазин
+                        </NavLink>
+                    </div>
+                </div>
+                <div className="col-sm-9">
+                    <Routes>
+                        <Route
+                            path="/profile/*"
+                            element={
+                                <Profile
+                                    function={props.function.key_getUser}
+                                />
+                            }
+                        />
+                        <Route path="/messages" element={<Messages />} />
+                        <Route path="/settings" element={<Settings />} />
+                        <Route
+                            path="/friends"
+                            element={
+                                <Friends
+                                    function={props.function.key_getUsers}
+                                />
+                            }
+                        />
+                        <Route
+                            path="/shop"
+                            element={
+                                <Shop function={props.function.key_getUsers} />
+                            }
+                        />
+                    </Routes>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
