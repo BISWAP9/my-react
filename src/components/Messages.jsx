93a8c8610632eeb1abeccbea93d3b28d78a39e7b
BISWAP9 <116273370+BISWAP9@users.noreply.github.com>
2022-12-03 10:03:29 +0300

    add css cursor pointer

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,4 +1,34 @@
+import pageImg from "../assets/img/2.jpg";
+import textBG from "../assets/img/bike1.JPEG"
+
 const Messages = () => {
-  return <h2>Диалоги</h2>;
+  return (
+    <>
+    <div className="row">
+      <div className="col-md-6">
+        <img src={pageImg} alt="" width={"100%"} />
+      </div>
+      <div className="col-md-6">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam quia eligendi ab ut! Soluta numquam quis cum cumque debitis nemo consequuntur libero repudiandae tempore aperiam, suscipit repellat deleniti, inventore dicta!
+        Architecto alias numquam molestias distinctio, nostrum voluptate dicta sit deserunt temporibus nihil nemo blanditiis porro, quibusdam eos perspiciatis, excepturi labore quisquam veniam! Veniam dignissimos non exercitationem? Porro itaque dolore modi.
+        Soluta velit dolorum eos quasi, deleniti, aperiam magnam consequatur fuga dolores tempora temporibus neque laudantium corrupti fugiat sapiente. Fugit modi consectetur magnam sit sint ex repellendus nam et quidem officiis.
+      </div>
+    </div>
+    <div className="row mt-4">
+      <div className="col-md-6 offset-6" style={{ backgroundImage: `url(${textBG})`, width: `1000px`, height: `1000px` }}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit sequi facere autem eius natus quisquam voluptatibus ducimus atque velit, illum ex reiciendis sunt accusamus dolore maxime consequuntur, nisi temporibus enim.
+        Blanditiis earum veniam quisquam pariatur corrupti molestias minus! Ut deleniti debitis incidunt laboriosam unde blanditiis. Soluta, accusantium consequuntur. Quibusdam minima nesciunt numquam fugit incidunt libero praesentium consequuntur nisi non corrupti.
+        Placeat repudiandae nihil debitis sunt ea minima ab distinctio nulla, possimus molestias id vel nobis assumenda neque adipisci impedit corrupti. Aliquid aut harum culpa! Neque modi quam laborum iste voluptas!
+        Culpa quas tempora, at quia distinctio ab quibusdam illum rerum perspiciatis error sed corporis et numquam officia minus nisi consectetur, ullam obcaecati nulla? Dignissimos harum aliquid aspernatur quam atque animi?
+        Quisquam, ducimus vel. Cum distinctio inventore reiciendis explicabo ea ipsa, maxime sed necessitatibus, nihil eligendi, repellat doloribus dolore illum. Voluptatum, ratione blanditiis. Quibusdam, laborum non delectus sapiente laudantium nisi nostrum!
+        Voluptatum repellendus placeat cumque praesentium ea nostrum molestias pariatur quod quibusdam molestiae consequuntur aliquid consectetur dolorum quo sit, aspernatur ipsa tenetur aliquam accusantium commodi quasi tempora unde saepe maxime? Cupiditate?
+        Distinctio explicabo nihil natus ducimus quod amet accusantium velit adipisci non, nemo eius beatae porro dolorem similique assumenda harum omnis odio quasi perferendis. Sint, tenetur. Corrupti consequatur esse hic commodi?
+        Expedita doloremque sit, reiciendis cupiditate reprehenderit commodi quod quae mollitia impedit autem! Voluptatibus enim quasi earum officia. Doloribus tenetur mollitia harum perferendis, suscipit quae beatae quo, dolor eveniet iste eligendi.
+        Possimus nihil natus incidunt sapiente laboriosam, voluptate laudantium, alias ad eveniet ipsam adipisci repudiandae facilis, perferendis dignissimos eum excepturi pariatur aperiam nam. Repudiandae atque, voluptatum unde explicabo eos omnis nisi!
+        Odio culpa quia eveniet. Quos excepturi adipisci fugiat corrupti ratione nemo aliquam error amet nobis quia natus suscipit labore voluptatem minus debitis officiis sit illo modi magni, odio odit delectus.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime magni, doloremque consequatur, similique natus ipsum blanditiis nulla nemo nesciunt, laborum temporibus molestias. Fugiat ab ullam deleniti dolorum libero maxime aperiam?
+      </div>
+    </div>
+    </>
+  );
 };
 export default Messages;
